567633354894e3d6e139d3453fdb6432b697f306
nl1031 <nongli1031@gmail.com>
2026-08-04 16:18:48 +0800

    Rename branch -tici; car_list Outback 2023; AGNOS 12.6 office match

--- launch_env.sh
+++ launch_env.sh
@@ -7,7 +7,7 @@ export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="12.8"
+  export AGNOS_VERSION="12.6"  # match office C3 flash; was 12.8 on master-tici
 fi
 
 export STAGING_ROOT="/data/safe_staging"
